60997049438ef7161178a44a763e818a791cc36e
shakyp <danilxshakurov@gmail.com>
2023-10-17 17:06:10 +0400

    Added method closeWord

--- game/Main.java
+++ game/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,11 +13,11 @@ public class Main {
 
     static Scanner scanner = new Scanner(System.in); // Сканер
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         startAndQuit();
     }
 
-    static void startAndQuit() {
+    static void startAndQuit() throws IOException {
         System.out.println("""
                 Введите букву "S" чтобы начать новую игру.
                 Чтобы выйти введите букву "Q".
@@ -35,9 +36,10 @@ public class Main {
         } while (key != 'S' & key != 'Q');
     } // Начало нового раунда/выход из игры
 
-    public static void gameStart() {
+    public static void gameStart() throws IOException {
         System.out.println("Вы нажали S");
         drawGallows();
+        closeWord();
     } // Начало игры
 
     public static void drawGallows() {
@@ -95,7 +97,7 @@ public class Main {
     |   |
     |  /
     +---+
-""",            """
+""", """
     +---+
     |   |
     |   O
@@ -109,9 +111,38 @@ public class Main {
     } // Рисование виселицы
 
     public static String getWords() throws IOException {
-
         List<String> words = Files.readAllLines(Paths.get("game/words.txt"), StandardCharsets.UTF_8);
-        int index = new Random().nextInt(words.size());
-        return words.get(index);
-    } // Отвечает за получение и обработку файла
+        return words.get(new Random().nextInt(words.size()));
+    } // Отвечает за получение слова из файла
+
+    public static void closeWord() throws IOException {
+        String words = getWords(); // Загаданное слово
+        System.out.println(words); // Вывод загаданого слова, нужно будет удалить
+        String[] chars = words.split(""); // Преобразование строки в массив букв
+
+        int charsLength = chars.length; // Длина массива
+        String myString = new String(new char[charsLength]).replace("\0", "❌"); // замена всех букв в строке на X
+        System.out.println("Загаданое слово: " + myString); // Вывод спрятаной строки
+
+        System.out.print("Введите букву: ");
+        char letter = scanner.next().charAt(0);
+
+        int startIndex = 0; // Начальный индекс строки
+        String strGuessLetters = ""; // Начальная строка для открывания букв
+        for (int i = 0; i < chars.length; i++) {
+            int indexLetter = words.indexOf(letter, startIndex);
+            if (indexLetter == -1) {
+                break;
+            } else {
+                if (startIndex == 0) {
+                    strGuessLetters = myString.substring(0, indexLetter) + Character.toUpperCase(letter) + myString.substring(indexLetter + 1);
+                } else {
+                    strGuessLetters = strGuessLetters.substring(0, indexLetter) + Character.toUpperCase(letter) + strGuessLetters.substring(indexLetter + 1);
+                }
+                startIndex = indexLetter + 1;
+            }
+        }// Цикл отвечает за замену символов ✖ на отгаданую букву
+        System.out.println("Загаданое слово: " + strGuessLetters);
+    } // Отвечает за скрытие слова и показа отгаданых букв
+
 }
